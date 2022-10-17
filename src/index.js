fce8696aa9a34d64d3894a9cb1882f6fbec7d288
Emre Karayaman <karayaman.emre@outlook.com>
2022-10-17 13:12:19 +0300

    add header
    Logo, navbar, and links added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import Logo from './assets/logo.png';
+
+const logo = new Image();
+logo.src = Logo;
+
+const logoDiv = document.getElementById('logo');
+logoDiv.appendChild(logo);
