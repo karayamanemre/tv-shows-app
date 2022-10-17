f0157b5e92a44819b3bbf76207268945a907057a
= <adedejifavour6@gmail.com>
2022-10-17 17:40:03 +0100

    Add popup handler function

--- src/modules/apiData.js
+++ src/modules/apiData.js
@@ -1,9 +1,11 @@
 import createList from './showList.js';
+import popUpHandler from './commentPopUp.js';
 
 const fetchData = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const shows = await response.json();
   const latestShows = shows.slice(0, 12);
   createList(latestShows);
+  popUpHandler(latestShows)
 };
 export default fetchData;
