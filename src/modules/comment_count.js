3bba2a9e75b836b6593a928d68c4b13618763ce5
= <adedejifavour6@gmail.com>
2022-10-19 09:51:07 +0100

    Install neccessary dependencies

--- src/modules/comment_count.js
+++
@@ -1,7 +0,0 @@
-const countComment = () => {
-  const commentItems = document.querySelectorAll('.comment_paragraph');
-  const commentsNum = commentItems.length;
-  return commentsNum;
-};
-
-export default countComment;
