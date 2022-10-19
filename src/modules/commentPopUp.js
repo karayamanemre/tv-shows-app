3a1209ec0f662e1ad649a8902976c60c673883b1
= <adedejifavour6@gmail.com>
2022-10-19 08:56:32 +0100

    Add comment count to the comment page

--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -1,12 +1,13 @@
 import closeIcon from '../assets/close.png';
 import addComment from './addComment.js';
+import countComment from './comment_count.js';
 import showComments from './fetchComment.js';
 
 const popUpPage = document.querySelector('.popup');
 
 const popUpHandler = (shows) => {
   const commentButton = document.querySelectorAll('.comment_button');
-  commentButton.forEach((button) => {
+  commentButton.forEach(async (button) => {
     button.addEventListener('click', async () => {
       const actualButton = shows.find((show) => show.id === +button.dataset.id);
       popUpPage.classList.add('visible');
@@ -17,15 +18,16 @@ const popUpHandler = (shows) => {
           <img class='close_icon' src='${closeIcon}'>
           <h2>${actualButton.name}</h2>
           <h4>Genres: ${actualButton.genres}</h4>
-          <div class='comment_section'>
           <h3>Comments (${comments.error ? 0 : comments.length})<h3/>
-          ${comments.error ? 'No comment for this movie' : `
+          <div class='comment_section'>
+          ${comments.error ? '<p class="no_comment"> No comment for this movie</p>' : ` 
             ${comments.map((comment, idx) => `
               <p class='comment_paragraph' key=${idx}>${comment.creation_date} ${comment.username}: "${comment.comment}"</p>
             `).join('')}
           </div>`
 }  
           <form class="form">
+            <h2 class="form_heading">Add Comment<h2/>
             <input type="text" class="username" placeholder="Your Name...">
             <textarea name="comment" class="comment" cols="30" rows="10" placeholder="Your Insight..."></textarea>
             <button type='button' class='submit_button'>Add Comment</button>
@@ -38,6 +40,7 @@ const popUpHandler = (shows) => {
       });
 
       addComment(button.dataset.id);
+      countComment();
     });
   });
 };
