50b7e63ffde8bd1beb0cbbadfe047dd7c8a93aec
Emre Karayaman <karayaman.emre@outlook.com>
2022-10-21 01:49:22 +0300

    change design
    - Colors changed.
    - Summary of the shows added on hover images.
    - Custom scrollbar added.
    - Button styles changed.

--- src/modules/showList.js
+++ src/modules/showList.js
@@ -5,13 +5,16 @@ const createList = (shows) => {
   shows.forEach((item, index) => {
     displayContainer.innerHTML += `
     <div class="card">
-      <img class="img" src="${item.image.medium}" alt="${item.name} poster">
+      <div class="img" style="background: url('${item.image.medium}'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+        <span>${item.summary}</span>
+      </div>
       <h3 class="title">${item.name}</h3>
       <div class="likes">
         <img class="like-icon" id='${item.id}' src="${LikeIcon}">
         <p class="like-count" id='${item.id}'>0</p><p>Likes</p>
       </div>
-      <button class="comment_button" data-id=${index + 1}>Comment</button>
+      <button class="comment_button" data-id=${index + 1}>
+        <span class="button-top">Comment</span></button>
     </div>
     `;
   });
